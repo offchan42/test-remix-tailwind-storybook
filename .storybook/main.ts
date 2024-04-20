efbe7698f4ec28dc7fc5badda91efbc0b4e342e7
Chanchana Sornsoontorn <off9955555@gmail.com>
2024-04-21 01:01:12 +0700

    Use different vite config file for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   stories: [
@@ -15,10 +17,22 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {
+      builder: {
+        // use different vite config for storybook: https://github.com/storybookjs/storybook/discussions/25519#discussioncomment-8064641
+        viteConfigPath: "vite.storybook.config.ts",
+      }
+    },
   },
   docs: {
     autodocs: "tag",
   },
+  // this is to allow storybook to import files from the app
+  // read more here: https://github.com/storybookjs/storybook/issues/18891#issuecomment-1228123054
+  // viteFinal: async (config, { configType }) => {
+  //   return mergeConfig(config, {
+  //     plugins: [tsconfigPaths()],
+  //   })
+  // },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
-const isStorybook = process.argv[1]?.includes("storybook");
-
 export default defineConfig({
-  // workaround to use storybook in Remix: https://github.com/storybookjs/storybook/discussions/25519#discussioncomment-8108937
-  plugins: [!isStorybook && remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths()],
 });
